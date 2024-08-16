4078b6a21820099ea42f0ed5273ff8fc07d40a3a
Scott O'Connor <scotty.who@gmail.com>
2024-08-17 08:43:55 +1200

    Possible fix for GitHub Actions

--- src/test/java/FontTest.java
+++ src/test/java/FontTest.java
@@ -1,13 +1,25 @@
+import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
 public class FontTest {
 
-    @Test
-    public void checkFont() {
-        FontSetter window = new FontSetter(); // Instantiate FontSetter to test
-        assertEquals("Consolas", FontSetter.consolas().getFontName());
+//    @Test
+//    public void checkFont() {
+//        FontSetter window = new FontSetter(); // Instantiate FontSetter to test
+//        assertEquals("Consolas", FontSetter.consolas().getFontName());
+//    }
+
+    // Method to return a text area's font name
+    public String checkFont(RSyntaxTextArea textArea) {
+        return textArea.getFont().getName();
     }
 
+    // Tests that ChildWindow uses the expected font
+    @Test
+    public void checkFont() {
+        ChildWindow window = new ChildWindow(); // Instantiate ChildWindow to test
+        assertEquals("Consolas", checkFont(ChildWindow.newPage));
+    }
 }
